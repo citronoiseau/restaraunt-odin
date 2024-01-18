48f1be384fce309a1fd3ba411c6739aeb0bb728b
citronoiseau <avroraka2004@gmail.com>
2024-01-18 12:36:43 +0200

    added active class toggle in the header

--- src/header.js
+++ src/header.js
@@ -5,16 +5,26 @@ export default function createHeader() {
   header.appendChild(navbar);
   const links = document.createElement("ul");
   navbar.appendChild(links);
-  const homeLink = document.createElement("li");
-  homeLink.classList.add("homeLink");
-  homeLink.textContent = "Home";
-  const menuLink = document.createElement("li");
-  menuLink.classList.add("menuLink");
-  menuLink.textContent = "Menu";
-  const contactLink = document.createElement("li");
-  contactLink.classList.add("contactLink");
-  contactLink.textContent = "Contact";
-  links.appendChild(homeLink);
-  links.appendChild(menuLink);
-  links.appendChild(contactLink);
+
+  const homeLink = createLink("Home", "homeLink", links);
+  homeLink.classList.add("active");
+  const menuLink = createLink("Menu", "menuLink", links);
+  const contactLink = createLink("Contact", "contactLink", links);
+
+  [homeLink, menuLink, contactLink].forEach((link) => {
+    link.addEventListener("click", () => {
+      [homeLink, menuLink, contactLink].forEach((link) => {
+        link.classList.remove("active");
+      });
+      link.classList.add("active");
+    });
+  });
+}
+
+function createLink(text, className, parent) {
+  const link = document.createElement("li");
+  link.classList.add(className);
+  link.textContent = text;
+  parent.appendChild(link);
+  return link;
 }
